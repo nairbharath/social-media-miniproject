d51038269cb249a54107a363379db6bf2cf4f991
nairbharath <nairbharath21@gmail.com>
2022-09-26 22:12:45 +0530

    1. setup navbar, login and signup buttons

---
+++ src/components/Layouts/RightContent/AuthButtons.tsx
@@ -0,0 +1,39 @@
+import { Button } from "@chakra-ui/react";
+import React from "react";
+import { useSetRecoilState } from "recoil";
+import { authModalState } from "../../../atoms/authModalAtom";
+
+//no props in this component
+
+const AuthButtons: React.FC = () => {
+  const setAuthModalState = useSetRecoilState(authModalState);
+  return (
+    <>
+      <Button
+        variant="outline"
+        height="28px"
+        display={{ base: "none", sm: "flex" }}
+        width={{ base: "70px", md: "110px" }}
+        mr={2}
+        onClick={() => setAuthModalState({ open: true, view: "login" })}
+      >
+        Log In
+      </Button>
+
+      {/* //display={{show nothing on mobile screen, small=flex}}
+       */}
+       {/* //by default solid no need to specify */}
+      <Button
+        variant="solid"
+        height="28px"
+        display={{ base: "none", sm: "flex" }}
+        width={{ base: "70px", md: "110px" }}
+        mr={2}
+        onClick={() => setAuthModalState({ open: true, view: "signup" })}
+      >
+        Sign Up
+      </Button>
+    </>
+  );
+};
+export default AuthButtons;
