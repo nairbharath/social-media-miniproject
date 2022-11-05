2740958a15a0c7f79ea04721e0b2ec398c1b8bdf
nairbharath <nairbharath21@gmail.com>
2022-11-05 20:52:04 +0530

    14. About component, Change community image

--- src/components/Posts/NewPostForm.tsx
+++ src/components/Posts/NewPostForm.tsx
@@ -28,6 +28,7 @@ import {
   } from "firebase/firestore";
   import { firestore, storage } from "../../firebase/clientApp";
   import { getDownloadURL, ref, uploadString } from "firebase/storage";
+import useSelectFile from "../../hooks/useSelectFile";
   
   type NewPostFormProps = {
     user: User;
@@ -68,7 +69,8 @@ import {
       title: "",
       body: "",
     });
-    const [selectedFile, setSelectedFile] = useState<string>();
+    // const [selectedFile, setSelectedFile] = useState<string>();
+    const { selectedFile, setSelectedFile, onSelectFile } = useSelectFile();
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
   
@@ -114,21 +116,21 @@ import {
   
     };
   
-    const onSelectImage = (event: React.ChangeEvent<HTMLInputElement>) => {
-      console.log("THIS IS HAPPENING", event);
+    // const onSelectImage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    //   console.log("THIS IS HAPPENING", event);
   
-      const reader = new FileReader();
+    //   const reader = new FileReader();
   
-      if (event.target.files?.[0]) {
-        reader.readAsDataURL(event.target.files[0]);
-      }
+    //   if (event.target.files?.[0]) {
+    //     reader.readAsDataURL(event.target.files[0]);
+    //   }
   
-      reader.onload = (readerEvent) => {
-        if (readerEvent.target?.result) {
-          setSelectedFile(readerEvent.target.result as string);
-        }
-      };
-    };
+    //   reader.onload = (readerEvent) => {
+    //     if (readerEvent.target?.result) {
+    //       setSelectedFile(readerEvent.target.result as string);
+    //     }
+    //   };
+    // };
   
     const onTextChange = (
       event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
@@ -166,7 +168,8 @@ import {
           {selectedTab === "Images & Video" && (
             <ImageUpload
               selectedFile={selectedFile}
-              onSelectImage={onSelectImage}
+              // onSelectImage={onSelectImage}
+              onSelectImage={onSelectFile}
               setSelectedTab={setSelectedTab}
               setSelectedFile={setSelectedFile}
             />
